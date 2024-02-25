256d9023feb90530912f926bc4cd9afd445289f9
Isaac-Leon1 <isaac.leon@epn.edu.ec>
2024-02-24 21:24:14 -0500

    Cambios en el diseño y vista de una tabla

--- src/Pag5_Compra.java
+++ src/Pag5_Compra.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -6,15 +8,52 @@ import java.sql.*;
 
 public class Pag5_Compra {
     private JTable table1;
-    private JButton FACTURARButton;
     private JButton REGRESARButton;
     private JFormattedTextField formattedTextField1;
     private JButton SELECCIONARButton;
     public JPanel pag5CompraPanel;
+    private JLabel imagenLabel;
+    private JTable table2;
+    private JButton ELIMINARButton;
     static JFrame frameCompra = new JFrame("Compra");
     ConexionDB conexionDB = new ConexionDB();
 
     public Pag5_Compra() {
+        table1.setDefaultEditor(Object.class, null);
+        actualizarTabla();
+        table1.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                if (!e.getValueIsAdjusting()) {
+                    int filaSeleccionada = table1.getSelectedRow();
+                    if (filaSeleccionada != -1) {
+                        obtenerImagen(filaSeleccionada);
+                    }
+                }
+            }
+        });
+
+        REGRESARButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frameCompra.dispose();
+                Pag1_Inicio.frame.setVisible(true);
+            }
+        });
+        /*FACTURARButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frameCompra.setVisible(false);
+                Pag5i1_Factura.frameFacturaCompra.setContentPane(new Pag5i1_Factura().pag5FacturaPanel);
+                Pag5i1_Factura.frameFacturaCompra.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                Pag5i1_Factura.frameFacturaCompra.setSize(800, 800);
+                Pag5i1_Factura.frameFacturaCompra.setVisible(true);
+                Pag5i1_Factura.frameFacturaCompra.setLocationRelativeTo(null);
+            }
+        });*/
+    }
+
+    public void actualizarTabla(){
         try {
             Connection connection = conexionDB.ConexionLocal();
 
@@ -41,24 +80,21 @@ public class Pag5_Compra {
         } catch (SQLException ex) {
             throw new RuntimeException(ex);
         }
-
-        REGRESARButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                frameCompra.dispose();
-                Pag1_Inicio.frame.setVisible(true);
+    }
+    public void obtenerImagen(int filaSeleccionada){
+        Connection connection = conexionDB.ConexionLocal();
+        try{
+            int idFilaSeleccionada= Integer.parseInt(table1.getModel().getValueAt(filaSeleccionada,0).toString());
+            PreparedStatement ps = connection.prepareStatement("SELECT imagen_prd FROM Productos WHERE producto_id = ?");
+            ps.setInt(1, idFilaSeleccionada);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()){
+                byte[] bytes = rs.getBytes("imagen_prd");
+                ImageIcon imageIcon = new ImageIcon(bytes);
+                imagenLabel.setIcon(imageIcon);
             }
-        });
-        FACTURARButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                frameCompra.setVisible(false);
-                Pag5i1_Factura.frameFacturaCompra.setContentPane(new Pag5i1_Factura().pag5FacturaPanel);
-                Pag5i1_Factura.frameFacturaCompra.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                Pag5i1_Factura.frameFacturaCompra.setSize(800, 800);
-                Pag5i1_Factura.frameFacturaCompra.setVisible(true);
-                Pag5i1_Factura.frameFacturaCompra.setLocationRelativeTo(null);
-            }
-        });
+        }catch (SQLException ex){
+            JOptionPane.showMessageDialog(null, ex.getMessage());
+        }
     }
 }
